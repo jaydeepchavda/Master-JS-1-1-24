004bfd946d9aff305d7550a08c19454e81a3af60
jaydeepchavda <jaydipchavda2216@gmail.com>
2024-01-02 23:17:34 +0530

    lexical commit

--- 006_recursion.js
+++ 006_recursion.js
@@ -432,3 +432,62 @@ object.
 
 To make our code future-proof and easier to understand, we should access properties of the global object directly, 
 as window.x.*/
+
+// Lexical Environment'
+
+/* 
+Step 1. Variables
+In JavaScript, every running function, code block {...}, and the script as a whole have an internal
+ (hidden) associated object known as the Lexical Environment.
+
+The Lexical Environment object consists of two parts:
+
+Environment Record – an object that stores all local variables as its properties 
+(and some other information like the value of this).
+A reference to the outer lexical environment, the one associated with the outer code.
+A “variable” is just a property of the special internal object, Environment Record.
+ “To get or change a variable” means “to get or change
+ a property of that object”.
+When the script starts, the Lexical Environment is pre-populated with all declared variables.
+Initially, they are in the “Uninitialized” state. That’s a special internal state, 
+it means that the engine knows about the variable, but it cannot be referenced until
+ it has been declared with let. It’s almost the same as if the variable didn’t exist.
+Then let phrase definition appears. There’s no assignment yet, so its value is undefined.
+ We can use the variable from this point forward.
+phrase is assigned a value.
+phrase changes the value.
+
+
+Step 2. Function Declarations
+A function is also a value, like a variable.
+
+The difference is that a Function Declaration is instantly fully initialized.
+
+When a Lexical Environment is created, a Function Declaration immediately 
+becomes a ready-to-use function (unlike let, that is unusable till the declaration).
+
+Step 3. Inner and outer Lexical Environment
+When a function runs, at the beginning of the call, a new Lexical Environment is created
+ automatically to store local variables and parameters of the call.
+
+
+ When the code wants to access a variable – the inner Lexical Environment is searched first,
+  then the outer one, then the more outer one and so on until the global one.
+
+
+  Step 4. Returning a function
+Let’s return to the makeCounter example.
+
+function makeCounter() {
+  let count = 0;
+
+  return function() {
+    return count++;
+  };
+}
+
+let counter = makeCounter();
+
+
+A variable is updated in the Lexical Environment where it lives.
+*/
