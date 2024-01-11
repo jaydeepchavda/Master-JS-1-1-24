33805c6931fb32ac6d9d96fedc454a284481380d
jaydeepchavda <jaydipchavda2216@gmail.com>
2024-01-11 23:02:46 +0530

    getter commit

--- 009_getterSetter.js
+++ 009_getterSetter.js
@@ -120,3 +120,68 @@ for (let key in user) {
   clone[key] = user[key]
 }
  */
+
+
+//    ----------       Property getters and setters
+
+/* 
+Getters and setters
+Accessor properties are represented by “getter” and “setter” methods. 
+In an object literal they are denoted by get and set:
+
+let obj = {
+  get propName() {
+    // getter, the code executed on getting obj.propName
+  },
+
+  set propName(value) {
+    // setter, the code executed on setting obj.propName = value
+  }
+};
+*/
+
+
+/* 
+The getter works when obj.propName is read, the setter – when it is assigned.
+
+For instance, we have a user object with name and surname:
+
+let user = {
+  name: "John",
+  surname: "Smith"
+};
+Now we want to add a fullName property, that should be "John Smith". 
+    Of course, we don’t want to copy-paste existing information, so we can implement it as an accessor:
+
+let user = {
+  name: "John",
+  surname: "Smith",
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  }
+};
+
+alert(user.fullName); // John Smith */
+
+
+/* 
+let user = {
+  name: "John",
+  surname: "Smith",
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  }
+};
+
+// set fullName is executed with the given value.
+user.fullName = "Alice Cooper";
+
+alert(user.name); // Alice
+alert(user.surname); // Cooper
+*/
