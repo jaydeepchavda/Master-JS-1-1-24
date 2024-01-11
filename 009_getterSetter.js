8f884630e1788a7ec2e6ce4205dfa9fac7f39a61
jaydeepchavda <jaydipchavda2216@gmail.com>
2024-01-11 23:08:40 +0530

    setter commit

--- 009_getterSetter.js
+++ 009_getterSetter.js
@@ -185,3 +185,42 @@ user.fullName = "Alice Cooper";
 alert(user.name); // Alice
 alert(user.surname); // Cooper
 */
+
+
+
+// Smarter getters/setters
+// Getters/setters can be used as wrappers over “real” property values to gain more control over operations with them.
+
+// For instance, if we want to forbid too short names for user, we can have a setter name and keep the 
+// value in a separate property _name:
+
+let usert = {
+  get name() {
+    return this._name;
+  },
+
+  set name(value) {
+    if (value.length < 4) {
+      console.log("Name is too short, need at least 4 characters");
+      return;
+    }
+    this._name = value;
+  }
+};
+
+usert.name = "Pete";
+console.log(user.name); // Pete
+
+usert.name = ""; // Name is too short...
+
+
+// Using for compatibility
+
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  let john = new User("John", 25);
+  
+  console.log( john.age ); // 25
